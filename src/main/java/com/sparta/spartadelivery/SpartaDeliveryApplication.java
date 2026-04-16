4077ff4596b4f4fa357b684b51a69b154806c03f
kimyubi <ieieie0419@gmail.com>
2026-04-16 16:10:52 +0900

    chore: initialize project

---
+++ src/main/java/com/sparta/spartadelivery/SpartaDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartadelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spartadelivery/SpartaDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartadelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
