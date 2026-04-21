2d86b1f9eb2f47eb3ea7d7290d63a0e802a13954
kimyubi <ieieie0419@gmail.com>
2026-04-21 11:59:49 +0900

    feat: AreaRepository에 운영 지역명 중복 검증 추가
    - AreaRepository에서 미삭제 운영 지역의 이름 중복 여부를 확인하도록 메서드 추가
    - deletedAt이 null인 데이터만 중복 검증 대상에 포함

--- src/main/java/com/sparta/spartadelivery/area/domain/repository/AreaRepository.java
+++ src/main/java/com/sparta/spartadelivery/area/domain/repository/AreaRepository.java
@@ -5,4 +5,6 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AreaRepository extends JpaRepository<Area, UUID> {
+
+    boolean existsByNameAndDeletedAtIsNull(String name);
 }
