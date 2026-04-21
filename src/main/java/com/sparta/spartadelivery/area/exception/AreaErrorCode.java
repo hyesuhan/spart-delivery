c3107691618e0e254bb52f6d941a70ca0c034fdc
kimyubi <ieieie0419@gmail.com>
2026-04-21 13:07:47 +0900

    feat: 운영 지역 수정 에러 코드 추가
    - AreaErrorCode에 운영 지역 수정 권한 없음 예외 추가
    - 등록/수정/삭제 API 관련 에러 코드를 주석으로 구분
    - 운영 지역 에러 메시지를 한국어로 정리

--- src/main/java/com/sparta/spartadelivery/area/exception/AreaErrorCode.java
+++ src/main/java/com/sparta/spartadelivery/area/exception/AreaErrorCode.java
@@ -10,6 +10,9 @@ public enum AreaErrorCode implements BaseErrorCode {
     AREA_CREATE_ACCESS_DENIED(HttpStatus.FORBIDDEN, "운영 지역을 등록할 권한이 없습니다."),
     DUPLICATE_AREA_NAME(HttpStatus.BAD_REQUEST, "이미 등록된 운영 지역명입니다."),
 
+    // 운영 지역 수정 API 관련 에러 코드
+    AREA_UPDATE_ACCESS_DENIED(HttpStatus.FORBIDDEN, "운영 지역을 수정할 권한이 없습니다."),
+
     // 운영 지역 삭제 API 관련 에러 코드
     AREA_DELETE_ACCESS_DENIED(HttpStatus.FORBIDDEN, "운영 지역을 삭제할 권한이 없습니다."),
     AREA_NOT_FOUND(HttpStatus.NOT_FOUND, "운영 지역을 찾을 수 없습니다.");
