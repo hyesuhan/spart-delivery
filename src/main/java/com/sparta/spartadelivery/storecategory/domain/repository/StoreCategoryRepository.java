91ddbaf501cfff8b891fbedf57fb33142273f92a
kimyubi <ieieie0419@gmail.com>
2026-04-22 14:35:50 +0900

    feat: 가게 카테고리 목록 조회 Repository 메서드 추가
    - 삭제되지 않은 가게 카테고리 목록을 Pageable 기반으로 조회하는 메서드를 추가합니다.
    - 등록 기능의 이름 중복 검증 메서드는 유지합니다.

--- src/main/java/com/sparta/spartadelivery/storecategory/domain/repository/StoreCategoryRepository.java
+++ src/main/java/com/sparta/spartadelivery/storecategory/domain/repository/StoreCategoryRepository.java
@@ -2,9 +2,13 @@ package com.sparta.spartadelivery.storecategory.domain.repository;
 
 import com.sparta.spartadelivery.storecategory.domain.entity.StoreCategory;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreCategoryRepository extends JpaRepository<StoreCategory, UUID> {
 
     boolean existsByNameAndDeletedAtIsNull(String name);
+
+    Page<StoreCategory> findAllByDeletedAtIsNull(Pageable pageable);
 }
