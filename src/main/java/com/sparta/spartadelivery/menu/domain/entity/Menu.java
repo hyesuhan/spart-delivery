e7e36721f630ad579f65a5c641e30dcd4b26e94e
조아영 <80755497+look516@users.noreply.github.com>
2026-04-25 01:41:32 +0900

    Revert "[FEAT] #11 메뉴 엔티티 추가"

--- src/main/java/com/sparta/spartadelivery/menu/domain/entity/Menu.java
+++ src/main/java/com/sparta/spartadelivery/menu/domain/entity/Menu.java
@@ -1,7 +1,6 @@
 package com.sparta.spartadelivery.menu.domain.entity;
 
 import com.sparta.spartadelivery.global.entity.BaseEntity;
-import com.sparta.spartadelivery.store.domain.entity.Store;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -20,13 +19,12 @@ public class Menu extends BaseEntity {
     @Column(name = "menu_id")
     private UUID id;
 
-    // 메뉴 판매 가게 (N : 1)
-    @Column(name = "store_id", nullable = false)
-    private UUID storeId;
-
-    // 메뉴 카테고리 (N : 1)
-    @Column(name = "menu_category_id", nullable = false)
-    private UUID menuCategoryId;
+    /*
+    // 메뉴 여러 개는 한 가게에 속할 수 있습니다. (N : 1)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+     */
 
     @Column(length = 100, nullable = false)
     private String name;
@@ -37,15 +35,6 @@ public class Menu extends BaseEntity {
     @Column(columnDefinition = "TEXT")
     private String description;
 
-    @Column(columnDefinition = "TEXT")
-    private String menuPictureUrl;
-
-    @Column(nullable = false)
+    @Column(name = "is_hidden", nullable = false)
     private Boolean isHidden = false;
-
-    @Column(columnDefinition = "TEXT")
-    private String aiDescription;
-
-    @Column(columnDefinition = "TEXT")
-    private String aiPrompt;
 }
