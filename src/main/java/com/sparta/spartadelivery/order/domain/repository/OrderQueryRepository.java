3c8997212879b2849e51027805ecf5b4bda6db92
t2025-m0284 <t2025-m0284@MacBook-Air.local>
2026-04-26 13:30:51 +0900

    feat: orderqueryrepository test code

--- src/main/java/com/sparta/spartadelivery/order/domain/repository/OrderQueryRepository.java
+++ src/main/java/com/sparta/spartadelivery/order/domain/repository/OrderQueryRepository.java
@@ -11,6 +11,7 @@ import com.sparta.spartadelivery.order.domain.entity.OrderStatus;
 import com.sparta.spartadelivery.order.domain.entity.QOrderItem;
 import com.sparta.spartadelivery.order.presentation.dto.request.OrderSearchRequest;
 import com.sparta.spartadelivery.order.presentation.dto.response.OrderSearch.OrderSearchResponse;
+import com.sparta.spartadelivery.store.domain.entity.QStore;
 import com.sparta.spartadelivery.user.domain.entity.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -22,6 +23,8 @@ import java.util.List;
 import java.util.UUID;
 
 import static com.sparta.spartadelivery.order.domain.entity.QOrder.order;
+import static com.sparta.spartadelivery.order.domain.entity.QOrderItem.orderItem;
+import static com.sparta.spartadelivery.store.domain.entity.QStore.store;
 
 
 @Repository
@@ -31,9 +34,11 @@ public class OrderQueryRepository {
 
     public Page<OrderSearchResponse> searchOrders(OrderSearchRequest.SearchCondition cond, Pageable pageable) {
         // 첫 번째 상품명만 가져오는 서브쿼리
+        QStore store = QStore.store;
         QOrderItem subItem = new QOrderItem("subItem");
 
 
+        /*
         Expression<String> firstItemNameExpression = ExpressionUtils.as(
                 JPAExpressions
                         .select(subItem.menuName) // OrderItem에 name 필드가 있다고 가정
@@ -42,22 +47,28 @@ public class OrderQueryRepository {
                 "firstItemName"
         );
 
+         */
+
         List<OrderSearchResponse> content = jpaQueryFactory
                 .select(Projections.constructor(OrderSearchResponse.class,
                         order.id,
                         order.customerId,
                         order.storeId,
+                        store.name,
                         order.status,
-                        firstItemNameExpression,
+                        orderItem.menuName.min(),
                         order.request,
                         order.createdAt
                 ))
                 .from(order)
+                .leftJoin(store).on(order.storeId.eq(store.id))
+                .leftJoin(order.orderItems, orderItem)
                 .where(
                         userIdEq(cond.userId(), cond.userRole()),
                         storeIdEq(cond.ownedStoreId(), cond.storeId()),
                         statusEq(cond.orderStatus())
                 )
+                .groupBy(order.id, store.name)
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .orderBy(order.createdAt.desc())
